59a7e6734ca90690eb85ea72e85f3bce52339973
xiejing <1766413580@qq.com>
2021-01-10 21:10:31 +0800

    高阶组件和高阶函数的概念

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -10,10 +10,14 @@ const Item=Form.Item;
 * 登录的路由组件
 * */
 export default class Login extends Component{
-    handleSubmit = (event) =>{
-
-    }
     render(){
+        //新组件传递给Form组件->强大的form对象
+        const form =this.props.form;
+        // const { getFieldDecorator } =form;高阶函数，返回一个组件
+        const onFinish = (event,values) => {
+            event.preventDefault();//阻止事件的默认行为
+            console.log('Received values of form: ', values);
+        };
         return(
             <div className="login">
                 <header className="login-header">
@@ -25,10 +29,11 @@ export default class Login extends Component{
                     <Form
                         name="normal_login"
                         className="login-form"
-                        onSubmit={this.handleSubmit}
+                        onFinish={onFinish}
                     >
                         <Item
-                            name="username"
+                            name="username"//标识名称
+                            //验证规则
                             rules={[{ required: true, message: 'Please input your Username!' }]}
                         >
                             <Input prefix={<UserOutlined className="site-form-item-icon" style={{color:'rgba(0,0,0,.25)'}} />} placeholder="Username" />
@@ -54,3 +59,35 @@ export default class Login extends Component{
         )
     }
 }
+/*
+* 1.高阶函数
+*   1）一类特别的函数
+*       a.接收函数类型的参数
+*       b.函数的返回值是函数
+*   2）常见的高阶函数
+*       a.定时器：setTimeOut()/setInterval()
+*       b.Promise：Promise(()=>{}) then(value=>{},reson=>{})
+*       c.数组遍历相关的方法;forEach()/filter()/map()/reduce()/find()/finndIndex()
+*       d.函数对象的bind()方法返回一个函数
+*       e.form.create()() / getFieldDecorator()() 返回一个函数
+* 一个函数代表一个动态的功能，而获取一个函数作为参数的高阶函数，更加动态更加具有扩展性
+*
+* 2.高阶组件
+*   1)本质是个函数，高阶组件也是高阶函数
+*   2）接收一个组件（被包装组件），返回一个新的组件（包装组件），包装组件会向被包装组件传入特定的属性
+*   3)作用：扩展组件的功能
+*   4）高阶组件也是高阶函数：接收一个组件函数，返回一个新的组件函数
+*
+* */
+//包装Form组件（内部有Form标签的组件）生成一个新的组件 Form(Login)
+//新组件会向Form组件传递一个强大的对象属性：form
+// const WrapLogin=Form.create()(Login);
+// export default WrapLogin;
+
+/*
+* 1.前台表单验证
+* 2.收集表单输入数据
+*
+*
+* */
+
